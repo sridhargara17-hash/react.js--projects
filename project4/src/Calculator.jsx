071022c729b90ef6b7448b708e65f36d0edcdf47
sridhar <sridhargara17@gmail.com>
2026-08-09 11:20:30 +0530

    project 4

---
+++ project4/src/Calculator.jsx
@@ -0,0 +1,49 @@
+import { formToJSON } from "axios";
+import React, { useState } from "react";
+import "./style.css"
+
+const Calculator = () => {
+  const [input, setInput] = useState("");
+  const display=(value)=>{setInput(input+value)}
+  const Calculate =()=>{setInput(eval(input))}
+  const clear = () => {
+    setInput("");
+  };
+
+  return (
+    <form className="calculator" name="calc">
+      <input type="text" className="value" value={input} />
+      <span
+        className="num clear"
+        onClick={() => {
+          clear();
+        }}
+      >
+        c
+      </span>
+      <span onClick={()=>display("/")}>/</span>
+      <span onClick={()=>display("*")}>*</span>
+      <span onClick={()=>display("7")}>7</span>
+      <span onClick={()=>display("8")}>8</span>
+      <span onClick={()=>display("9")}>9</span>
+      <span onClick={()=>display("-")}>-</span>
+      <span onClick={()=>display("4")}>4</span>
+      <span onClick={()=>display("5")}>5</span>
+      <span onClick={()=>display("6")}>6</span>
+      <span className="plus" onClick={()=>display("+")}>+</span>
+
+
+      <span onClick={()=>display("1")}>1</span>
+      <span onClick={()=>display("2")}>2</span>
+      <span onClick={()=>display("3")}>3</span>
+      <span onClick={()=>display("0")}>0</span>
+      <span onClick={()=>display("00")}>00</span>
+      <span onClick={()=>display(".")}>.</span>
+      <span className="num equal" onClick={()=>Calculate()}>=</span>
+      
+
+    </form>
+  );
+};
+
+export default Calculator;
