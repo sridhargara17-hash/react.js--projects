94f4901f9881cfebafe5d00dbe2fb22e8ca911f9
sridhar <sridhargara17@gmail.com>
2026-08-09 22:50:19 +0530

    project 5

---
+++ project5/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import ToggleBackGround from './ToggleBackGround.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+<ToggleBackGround />
+  </StrictMode>,
+)
